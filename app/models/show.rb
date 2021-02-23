4053dde0c2c38a8a4aaf7c3b3b2dd274b845c2b9
Samuel DeSantis <samuel.i.desantis@gmail.com>
2021-02-23 05:04:32 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,8 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    self.maximum(:rating).first
-    #self.where("rating = ?", self.highest_rating).first
+    self.where("rating = ?", self.highest_rating).first
   end
 
   def self.lowest_rating
