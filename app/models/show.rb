688770f0496078abed4d66a2ab39a097f6d7dc0d
Samuel DeSantis <samuel.i.desantis@gmail.com>
2021-02-23 04:51:35 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,27 @@
 class Show < ActiveRecord::Base
 
+  def initialize()
+  end
+
+  def highest_rating
+  end
+
+  def most_popular_show
+  end
+
+  def lowest_rating
+  end
+
+  def least_popular_show
+  end
+
+  def ratings_sum
+  end
+
+  def popular_shows
+  end
+
+  def shows_by_alphabetical_order
+  end
+
 end
